18caeebe9225631bd55bbd73724abfec3a1caa22
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-07-12 17:33:08 +0800

    Implement GreetManyTimes method

--- greet/greet_server/greetImpl.go
+++ greet/greet_server/greetImpl.go
@@ -3,6 +3,9 @@ package main
 import ( 
 	"context"
 	"log"
+	"strconv"
+	"time"
+
 	"github.com/wendysanarwanto/go-grpc-course/greet/greetpb"	
 )
 
@@ -17,4 +20,22 @@ func (*server) Greet(ctx context.Context, req *greetpb.GreetRequest) (*greetpb.G
 	}
 
 	return res, nil
+}
+
+func (*server) GreetManyTimes(req *greetpb.GreetManyTimesRequest, stream greetpb.GreetService_GreetManyTimesServer) error {
+	log.Printf("[INFO] GreetManyTimes function was invoked with %v", req)
+	// Send greet 10 times
+	firstName := req.GetGreeting().GetFirstName()
+	lastName := req.GetGreeting().GetLastName()
+	numGretings := int(req.GetNumGreetings())
+
+	for i:=1; i<=numGretings; i++ {
+		result := "Hello, "+firstName+" "+lastName+". Number: "+ strconv.Itoa(i)
+		res := &greetpb.GreetManyTimesResponse{
+			Result: result,
+		}
+		stream.Send(res)
+		time.Sleep(200 * time.Millisecond)
+	}
+	return nil
 }
